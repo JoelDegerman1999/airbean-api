42ed7b78e7f1b4cd1beeb1fd4e98f7963f88ec49
Joel Degerman <joel@joeldegerman.se>
2020-05-27 15:06:01 +0200

    Inital commit

---
+++ src/main/java/se/air/airbean/airbean/AirbeanApplication.java
@@ -0,0 +1,13 @@
+package se.air.airbean.airbean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbeanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirbeanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/air/airbean/airbean/AirbeanApplicationTests.java
@@ -0,0 +1,13 @@
+package se.air.airbean.airbean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbeanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
